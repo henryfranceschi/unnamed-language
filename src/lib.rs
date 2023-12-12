f18415a168e3a447fcb6329dca45def9f40ebd69
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2023-12-12 16:30:55 +0100

    removed Lookahead iterator adapter

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,1 @@
-pub mod lookahead;
 pub mod parser;
--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,17 @@
-use crate::lookahead::Lookahead;
 use self::scanner::Scanner;
 
 mod cursor;
 pub mod scanner;
 pub mod token;
 
-struct Parser<'a> {
-    iter: Lookahead<2, Scanner<'a>>,
+pub struct Parser<'a> {
+    scanner: Scanner<'a>,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(source: &'a str) -> Self {
-        Self { iter: Lookahead::new(Scanner::new(source)) }
+        Self {
+            scanner: Scanner::new(source),
+        }
     }
 }
