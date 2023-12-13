099913f98d347e3be2f3786dc12090f7ca1cf8b4
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2023-12-13 20:47:34 +0100

    removed scanner debugging code

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,6 @@ use unnamed_language::parser::scanner::Scanner;
 fn main() {
     for line in std::io::stdin().lock().lines() {
         let line = line.unwrap();
-        let mut scanner = Scanner::new(&line);
-
-        loop {
-            match scanner.scan() {
-                Ok(tok) => {
-                    let span = tok.span();
-                    let kind = format!("{:?}", tok.kind());
-                    let max = (line.len().max(1) as f32).log10() as usize + 1;
-                    println!(
-                        "[{:>max$}..{:<max$}] {:<12} | \"{}\"",
-                        span.start(),
-                        span.end(),
-                        kind,
-                        span.slice(),
-                        max = max
-                    );
-
-                    if tok.is_eof() {
-                        break;
-                    }
-                }
-                Err(err) => {
-                    eprintln!("lexical error: \"{}\"", err.message);
-                    let line_number = err.span.line_number();
-                    let column_number = err.span.column_number();
-                    let line = &line.lines().nth(line_number - 1).unwrap();
-
-                    eprintln!("[{line_number}:{column_number}] {line}");
-                }
             }
         }
     }
