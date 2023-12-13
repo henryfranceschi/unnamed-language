69cf0977bb4cb4dca7bb23dafb5ec45378acb285
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2023-12-13 23:27:20 +0100

    added wip cli

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
-use std::io::BufRead;
+use std::path::Path;
 
-use unnamed_language::parser::scanner::Scanner;
+use unnamed_language::parser::Parser;
 
 fn main() {
-    for line in std::io::stdin().lock().lines() {
-        let line = line.unwrap();
-            }
-        }
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() > 2 {
+        eprintln!("usage: {} [filename]", env!("CARGO_BIN_NAME"));
+        return;
     }
+
+    if let Some(path) = args.get(1).map(Path::new) {
+        run_from_file(path);
+    } else {
+        repl();
+    }
+}
+
+fn repl() {
+    todo!()
+}
+
+fn run_from_file(path: &Path) {
+    todo!()
+}
+
+fn run(source: String) {
+    Parser::new(&source);
 }
